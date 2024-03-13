7b3a98a7a6cd5e77e29f76e12f13f9fcbf61491d
guilherme godoy <guilhermesachinelli.564@gmail.com>
2024-03-13 15:26:49 -0300

    feat: create profile screen

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,10 +1,10 @@
 import { View } from 'react-native'
 import { Title } from '../../components/Title'
 import styles from './styles'
-export default function Home() {
+export default function Profile() {
   return (
     <View style={styles.container}>
-        <Title title="Home" />
+        <Title title="Profile" />
     </View>
   )
 }
